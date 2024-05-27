714bceb4ca013d7d27deb232cb2a03ca8706849e
Yagiz <uluocak.yagiz1@gmail.com>
2024-05-28 01:37:58 +0300

    uppdatemenu

--- classes/menu.php
+++ classes/menu.php
@@ -2,14 +2,13 @@
 
 class Menu extends AbstractModel{
     protected $table = 'menu';
-
+    private $id;
     private $product;
     private $category;
     private $price;
     private $query;
 
-    public function addMenu(){
-
+    public function postMenu(){
         // Verileri POST isteğinden al ve sanitize et
         $this->product = htmlspecialchars($_POST['product']);
         $this->category = htmlspecialchars($_POST['category']);
@@ -19,8 +18,16 @@ class Menu extends AbstractModel{
             echo "Geçersiz Fiyat Değeri.";
             return false;
         }
+        return true;
+    }
+
+    public function addMenu(){
+
+        if(!$this->postMenu()){
+            return false;
+        }
         // SQL sorgusunu hazırla
-        $this->query = "INSERT INTO menu(product_name, category, price) VALUES (:product_name, :category, :price)";
+        $this->query = "INSERT INTO menu(product, category, price) VALUES (:product, :category, :price)";
         $stmt = $this->db->prepare($this->query);
         $stmt->bindParam(':product', $this->product);
         $stmt->bindParam(':category', $this->category);
@@ -30,6 +37,26 @@ class Menu extends AbstractModel{
             return $stmt->execute();
         }catch(PDOException $e){
             echo "Menü Eklenirken Bir Hata Oluştu: " . $e->getMessage();
+            return false;
+        }
+    }
+    public function updateMenu(){
+        $this->id = $_GET['menu_id'];
+        if (!$this->postMenu()) {
+            return false;
+        }
+
+        $this->query = "UPDATE menu SET product=:product, category=:category, price=:price WHERE menu_id=:id";
+        $stmt = $this->db->prepare($this->query);
+        $stmt->bindParam(':id', $this->id);
+        $stmt->bindParam(':product', $this->product);
+        $stmt->bindParam(':category', $this->category);
+        $stmt->bindParam(':price', $this->price);
+
+        try{
+            return $stmt->execute();
+        }catch(PDOException $e){
+            echo "Güncelleme Sırasında Bir Hata Oluştu: " . $e->getMessage();
             return false;
         }
     }
